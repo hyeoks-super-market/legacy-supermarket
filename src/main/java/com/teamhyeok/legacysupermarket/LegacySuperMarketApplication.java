9765fe20bbde49e3010f04157a4f8e217554ec34
hyeoKing <cksgurwkd12@naver.com>
2024-07-07 19:10:44 +0900

    [#1] first legacy-super-market project settings. I hope to be a nice journey

---
+++ src/main/java/com/teamhyeok/legacysupermarket/LegacySuperMarketApplication.java
@@ -0,0 +1,13 @@
+package com.teamhyeok.legacysupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacySuperMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacySuperMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamhyeok/legacysupermarket/LegacySuperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhyeok.legacysupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacySuperMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
